5224c5d18bb88af12f036f7f8c692da53a133f76
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-06-20 08:58:12 +0545

    adding types to functions parameter, return value types

--- 07_adding_types_to_function_parameter.ts
+++ 07_adding_types_to_function_parameter.ts
@@ -1,1 +1,16 @@
 // Assigning type to variable is extremly common
+
+// Assigning function parameter type have some rules
+//1.return type of this function will this infered type returned by return keyword
+function add(a: number, b: number) {
+  return a + b;
+}
+
+// 2. We also can define/explicitly set return type by fun() : return type
+function adding(a: { val: number }, b: string): number {
+  return a.val + parseInt(b);
+}
+// 3. if it doesn't return anything , return type is void by default
+function adder(a: number, b: string) {
+  console.log(`${a.toString()} with ${b}`);
+}
